02440b39fa70ff7ed388fca3be4caf45db5015f8
Luancria <luanwilliandonascimento@gmail.com>
2026-04-22 16:24:23 -0300

    lacos-de-repeticao

---
+++ 1/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Loop que começa em 1, vai até 10 e aumenta de 1 em 1
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i); // Exibe o número atual no console
+            }
+        }
+    }
+}
